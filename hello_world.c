1991c4ee2416e1ff687b1941b72076d648bbf71a
JosepUPC <rchuguetgironjosep@gmail.com>
2016-02-17 17:54:55 +0100

    Josepprintf
    Edited printf, now it also outputs "Josep".

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-	printf("Hello my name is David");
+	printf("Hello my name is David\n");
 	//aqui lo vuestro
-
+	printf("Hello my name is Josep Huguet :)\n");
+	system("pause");
+	return 0;
 }
