06c0fb9b8a3dfe98e2f6013f2aeb56f2be181344
dafral <dafral1996@gmail.com>
2016-02-17 20:43:24 +0100

    Quitar comentario

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-	printf("Hello my name is David\n");
-	//aqui lo vuestro
+	printf("Hello my name is David Franco\n");
 	printf("Hello my name is Josep Huguet :)\n");
 	printf("Hello my name is Enrique I. Molina\n");
 	printf("Hello my name is Roger Busquets\n");
