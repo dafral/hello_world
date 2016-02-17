f54ac6ad34812ba9b5d045a580d18883595dacc1
QuiqueUPC <terrydkcontact@gmail.com>
2016-02-17 18:38:42 +0100

    \n
    :)

--- hello_world.c
+++ hello_world.c
@@ -4,7 +4,7 @@ int main(void){
 	printf("Hello my name is David\n");
 	//aqui lo vuestro
 	printf("Hello my name is Josep Huguet :)\n");
-	printf("Hello my name is Enrique I. Molina");
+	printf("Hello my name is Enrique I. Molina\n");
 
 	system("pause");
 	return 0;
