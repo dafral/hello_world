88105b25d54ce4b551fd344c896bb7cc57017d61
QuiqueUPC <terrydkcontact@gmail.com>
2016-02-17 18:29:40 +0100

    Add Quique
    añadido - printf("my name");

--- hello_world.c
+++ hello_world.c
@@ -4,6 +4,8 @@ int main(void){
 	printf("Hello my name is David\n");
 	//aqui lo vuestro
 	printf("Hello my name is Josep Huguet :)\n");
+	printf("Hello my name is Enrique I. Molina");
+
 	system("pause");
 	return 0;
 }
